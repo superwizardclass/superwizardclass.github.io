f65bebb88d06eb037efacd3745ae7a99741f319e
Dan S <wizardclassio@gmail.com>
2023-09-22 23:00:55 -0700

    Cleared create-vue boilerplate. Installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{vue,html,js}",
+    "./src/components/**/*.{vue,html,js}",
+    "./index.html"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
