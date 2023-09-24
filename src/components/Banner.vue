faa6e11a7a6039b74f80861263b81feada0c1233
Dan S <wizardclassio@gmail.com>
2023-09-23 20:28:39 -0700

    Created initial components to be used.

---
+++ src/components/Banner.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-l text-center">hello world.</h1>
+    <h2 class="text-xl text-center">my name is dan.</h2>
+  </div>
+</template>
+<style scoped>
+</style>
