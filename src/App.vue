5fd5a724ee0419fd0f731462879a3e24d384b8d3
Dan S <wizardclassio@gmail.com>
2023-09-23 20:30:00 -0700

    Applied background. Flex. Project in a working state.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-
+import Banner from './components/Banner.vue' 
+import Github from './components/Github.vue'
+import Linkedin from './components/Linkedin.vue'
 </script>
 
 <template>
-  
+<div class="bg-app-background bg-no-repeat bg-cover min-h-screen pt-20 flex flex-col font-cabin">
+<Banner></Banner>
+<Github></Github>
+<Linkedin></Linkedin>
+</div>
 </template>
 
 <style scoped>
