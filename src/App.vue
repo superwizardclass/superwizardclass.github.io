aadb23ba14e08bc0ca37bf2d78e235989753e9cf
Dan S <wizardclassio@gmail.com>
2023-09-24 00:15:30 -0700

    Fetch github api to populate username,avatar url, and last push time and data

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
+import { computed, onBeforeMount, onMounted, ref } from 'vue'
 import Banner from './components/Banner.vue' 
 import Github from './components/Github.vue'
 import Linkedin from './components/Linkedin.vue'
+
+const githubData = ref('')
+const API_URL = 'https://api.github.com/users/superwizardclass/events'
+const fetchGithubData = async (url) => {
+  const response = await fetch(url)
+  const githubJson = await response.json()
+  githubData.value = githubJson
+}
+
+onMounted(() => {
+  fetchGithubData(API_URL)
+})
+
 </script>
 
 <template>
 <div class="bg-app-background bg-no-repeat bg-cover min-h-screen pt-20 px-1 flex flex-col gap-2 font-cabin">
 <Banner></Banner>
 <div class="h-8"></div>
-<Github></Github>
+<Github :data="githubData"></Github>
 <Linkedin></Linkedin>
 </div>
 </template>
