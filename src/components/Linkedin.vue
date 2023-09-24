e82406ad7fdbdac75ccd42302b164c2bc8109ee8
Dan S <wizardclassio@gmail.com>
2023-09-24 00:18:00 -0700

    Linkedin component and Github components have hover and transitions added.

--- src/components/Linkedin.vue
+++ src/components/Linkedin.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <div class="container rounded-2xl py-4 mx-auto bg-stone-800 flex items-center flex-row justify-center gap-4  text-neutral-200">
+    <div class="container rounded-2xl py-4 mx-auto transition-all bg-opacity-90 hover:bg-opacity-100 bg-stone-800 flex items-center flex-row justify-center gap-4  text-neutral-200">
         
         <div class="">
             <a class="text-center" href="https://www.linkedin.com/in/dan3000/">Dan S.</a>
