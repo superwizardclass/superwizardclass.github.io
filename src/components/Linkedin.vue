211674fa7a00ec2ee04e82547cad1ed00ed0c6df
Dan S <wizardclassio@gmail.com>
2023-09-23 21:50:43 -0700

    Linkedin component copied from Github. Still in progress. Want to add hover and transition effects.

--- src/components/Linkedin.vue
+++ src/components/Linkedin.vue
@@ -1,6 +1,13 @@
 <script setup>
 </script>
 <template>
-
+    <div class="container rounded-2xl py-4 mx-auto bg-stone-800 flex items-center flex-row justify-center gap-4  text-neutral-200">
+        
+        <div class="">
+            <a class="text-center" href="https://www.linkedin.com/in/dan3000/">Dan S.</a>
+            <p class="text-xs ">Software Developer</p>
+        </div>
+        <img class="w-8" src="../components/icons/LI-In-Bug.png">
+    </div>
 </template>
 <style scoped></style>
