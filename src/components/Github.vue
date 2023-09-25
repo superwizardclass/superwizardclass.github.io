555f95d00651a8492b0be15f8e2e641fbf4db0b9
Dan S <wizardclassio@gmail.com>
2023-09-24 21:31:42 -0700

    Fixed early props passing via conditional rendering.

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -1,18 +1,25 @@
 <script setup>
-import { ref, defineProps } from 'vue'
+import { ref, computed } from 'vue'
+import moment from 'moment';
 
-const props = defineProps(['data'])
+const props = defineProps(['githubData'])
+
+
+const convertedDate = computed(() => {
+        const dateString = props.githubData[0]['created_at']
+        return moment(dateString).format('MMM Do YYYY')
+})
+
 
 </script>
 <template>
-    <div class="container rounded-2xl py-4 mx-auto bg-stone-800 hover:bg-violet-500 bg-opacity-90 transition-all flex items-center flex-row justify-center gap-4  text-neutral-200">
-        <img class="w-8 rounded-full" :src="data[0]['actor']['avatar_url']">
-        <div v-if="data !== 'undefined'" class="">
-            <a class="text-center hover:text-stone-800 transition-all" href="https://github.com/superwizardclass">{{ data[0]['actor']['display_login'] }}</a>
-            <p class="text-xs ">Last Active Time: </p>
+    <a href="https://github.com/superwizardclass" class="container rounded-2xl py-4 mx-auto max-w-xl transition-all bg-opacity-70 hover:bg-opacity-100 bg-stone-800 hover:bg-cyan-900 flex items-center flex-row justify-center gap-4 lg:gap-6 text-neutral-200">
+        <img class="w-8 lg:w-10 rounded-full" :src="githubData[0]['actor']['avatar_url']">
+        <div class="">
+            <p class="transition-all lg:text-lg" >{{ githubData[0]['actor']['display_login'] }}</p>
+            <p class="text-xs lg:text-sm">Last Activity: {{ convertedDate }}</p>
         </div>
-        <h1 v-else>Loading...</h1>
-        <img class="w-8" src="../components/icons/github-mark-white.png">
-    </div>
+        <img class="w-8 lg:w-10" src="../components/icons/github-mark-white.png">
+    </a>
 </template>
 <style scoped></style>
