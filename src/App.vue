a93c7828527c0d75af3f5dba65a09c2557d8c503
Dan S <wizardclassio@gmail.com>
2023-10-09 19:41:28 -0700

    Moved blog link to the top

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,13 @@ onMounted(() => {
     class="bg-app-background bg-no-repeat bg-cover bg-left lg:bg-left transition-all min-h-screen pt-20 px-1 flex flex-col gap-2 lg:gap-4 font-cabin">
     <Banner></Banner>
     <div class="h-8 lg:h-14"></div>
+    <Info :url="BLOG_URL">
+      <img class="w-12 lg:w-16 rounded-full" src="./assets/blog-profile-pic.jpg" />
+      <div class="">
+        <p class="font-bold text-lg lg:text-xl text-center">blog one <span class="text-2xl">&#128640;</span></p>
+        <p class="text-sm lg:text-base text-neutral-300">package may not include real life rocket.</p>
+      </div>
+    </Info>
     <Info v-if="githubData" :url="GH_URL">
       <img class="w-10 lg:w-12 rounded-full" :src="githubData[0]['actor']['avatar_url']">
       <div class="">
@@ -50,13 +57,6 @@ onMounted(() => {
         <p class="text-xs lg:text-base text-neutral-300">Software Developer</p>
       </div>
       <img class="w-10 lg:w-14" src="./assets/LI-In-Bug.png" />
-    </Info>
-    <Info :url="BLOG_URL">
-      <img class="w-12 lg:w-16 rounded-full" src="./assets/blog-profile-pic.jpg" />
-      <div class="">
-        <p class="font-bold text-lg lg:text-xl text-center">blog one <span class="text-2xl">&#128640;</span></p>
-        <p class="text-sm lg:text-base text-neutral-300">package may not include real life rocket.</p>
-      </div>
     </Info>
   </div>
 </template>
